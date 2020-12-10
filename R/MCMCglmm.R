ab062ce4d0b533dc6dc805ad41265ddfb127a671
jarrodhadfield <j.hadfield@ed.ac.uk>
2020-12-10 18:51:09 +0000

    better liability starting values for zibinomial models when size>1

--- R/MCMCglmm.R
+++ R/MCMCglmm.R
@@ -1025,9 +1025,9 @@
                 v<-diag(GRprior[[nG+nR]]$V)[length(diag(GRprior[[nG+nR]]$V))]
               }else{
                  data_tmp<-data_tmp[-which(data_tmp$MCMC_y==0),]  
-                 m1<-summary(glm(cbind(MCMC_y, MCMC_y.additional)~1, family="quasibinomial", data=data_tmp))
-                 v<-abs(((as.numeric(m1$dispersion[1])-0.5)/2)^2)
-                 mu<-as.numeric(m1$coef[1])
+                 m1<-summary(glm(cbind(MCMC_y, MCMC_y.additional-MCMC_y)~1, family="quasibinomial", data=data_tmp))
+                 v<-abs(((as.numeric(m1$dispersion[1])-1)/(mean(data_tmp$MCMC_y.additional)-1))/(plogis(m1$coef[1])*(1-plogis(m1$coef[1]))))
+                 mu<-m1$coef[1]*sqrt(1 + v*((16 * sqrt(3))/(15 * pi))^2) 
               }
             }
             if(family_set=="zitobit"){
