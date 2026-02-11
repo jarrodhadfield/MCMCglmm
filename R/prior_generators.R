1eb9ca7263b296c333eb384413ef20f91dc7a961
Jarrod Hadfield <j.hadfield@ed.ac.uk>
2026-02-11 07:35:27 +0000

    multinomial book updates

--- R/prior_generators.R
+++ R/prior_generators.R
@@ -2,12 +2,12 @@ IW <- function(V = 1, nu = 0.002) {
 
   f<-function(k, vtype){
 
-    if(!vtype%in%c("idh", "us", "idv")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
+    if(!vtype%in%c("idh", "us", "idv", "idvm", "idhm")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
     
-    if(vtype=="us" | vtype=="idv"){
+    if(vtype=="us" | vtype=="idv" | vtype=="idvm"){
       return(list(V=diag(k)*(V*nu)/(nu+k-1), nu=nu+k-1))
     }
-    if(vtype=="idh"){
+    if(vtype=="idh" | vtype=="idhm"){
        return(list(V=diag(k)*V, nu=nu))
     }    
   }
@@ -19,14 +19,14 @@ IG <- function(shape = 0.001, scale = 0.001) {
 
   f<-function(k, vtype) {
 
-    if(!vtype%in%c("idh", "us", "idv")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
+    if(!vtype%in%c("idh", "us", "idv", "idvm", "idhm")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
 
   	nu<-shape*2
     V<-2*scale/nu
-    if(vtype=="us" | vtype=="idv"){
+    if(vtype=="us" | vtype=="idv" | vtype=="idvm"){
       return(list(V=diag(k)*(V*nu)/(nu+k-1), nu=nu+k-1))
     }
-    if(vtype=="idh"){
+    if(vtype=="idh" | vtype=="idhm"){
       return(list(V=diag(k)*V, nu=nu))
     }   
   }
@@ -38,13 +38,13 @@ F <- function(df2 = 1, scale = 1000) {
 
   f<-function(k, vtype) {
 
-    if(!vtype%in%c("idh", "us", "idv")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
+    if(!vtype%in%c("idh", "us", "idv", "idvm", "idhm")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
     
     nu<-df2+k-1
-    if(vtype=="us" | vtype=="idv"){
+    if(vtype=="us" | vtype=="idv" | vtype=="idvm"){
       return(list(V=diag(k)*df2/nu, nu=nu, alpha.mu=rep(0,k), alpha.V=diag(k)*scale))
     }
-    if(vtype=="idh"){
+    if(vtype=="idh" | vtype=="idhm"){
       return(list(V=diag(k), nu=df2, alpha.mu=rep(0,k), alpha.V=diag(k)*scale))
     }   
 
@@ -58,13 +58,13 @@ tSD <- function(df = 1, scale = sqrt(1000)) {
   
   f<-function(k, vtype) {
   
-    if(!vtype%in%c("idh", "us", "idv")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
+    if(!vtype%in%c("idh", "us", "idv", "idvm", "idhm")){stop(paste("prior_generator not yet implemented for", vtype, "structures"))}
     	
   	nu<-df+k-1
-    if(vtype=="us" | vtype=="idv"){
+    if(vtype=="us" | vtype=="idv" | vtype=="idvm"){
       return(list(V=diag(k)*df/nu, nu=nu, alpha.mu=rep(0,k), alpha.V=diag(k)*scale^2))
     }
-    if(vtype=="idh"){
+    if(vtype=="idh" | vtype=="idhm"){
       return(list(V=diag(k), nu=df, alpha.mu=rep(0,k), alpha.V=diag(k)*scale^2))
     }
   }
